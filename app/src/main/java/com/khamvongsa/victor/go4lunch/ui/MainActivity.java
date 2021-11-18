483d35e91bc32a180afd180a1ea64eb09ec5bdb2
Victor <khamvongsa.v@hotmail.fr>
2021-11-18 15:07:03 +0100

    bottomnavigationView

---
+++ app/src/main/java/com/khamvongsa/victor/go4lunch/ui/MainActivity.java
@@ -0,0 +1,35 @@
+package com.khamvongsa.victor.go4lunch.ui;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.khamvongsa.victor.go4lunch.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initialize Bottom Navigation View.
+        BottomNavigationView navView = findViewById(R.id.bottom_navigation_view);
+
+        //Pass the ID's of Different destinations
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_map_view, R.id.navigation_list_view, R.id.navigation_workmates)
+                .build();
+
+        //Initialize NavController.
+        NavController navController = Navigation.findNavController(this, R.id.navHostFragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+
+    }
+}
