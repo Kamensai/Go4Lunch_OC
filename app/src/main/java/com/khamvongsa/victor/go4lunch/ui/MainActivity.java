3188ed04bf3800dcca58bef17abb9fcd946c982d
Victor <khamvongsa.v@hotmail.fr>
2021-11-26 18:02:59 +0100

    NavigationDrawer Affichage

--- app/src/main/java/com/khamvongsa/victor/go4lunch/ui/MainActivity.java
+++ app/src/main/java/com/khamvongsa/victor/go4lunch/ui/MainActivity.java
@@ -1,11 +1,20 @@
 package com.khamvongsa.victor.go4lunch.ui;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.Window;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.khamvongsa.victor.go4lunch.R;
 
+import java.util.Objects;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -13,23 +22,44 @@ import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
+    //BottomNavigationView
+    BottomNavigationView mBottomNavigationView;
+    AppBarConfiguration mAppBarConfiguration;
+    NavController mNavController;
+
+    //NavigationDrawer
+    DrawerLayout mDrawerLayout;
+    NavigationView mNavigationView;
+    Toolbar mToolbar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Initialize NavigationDrawer.
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mNavigationView = findViewById(R.id.navigation_drawer_view);
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        // NavigationDrawer menu.
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout,mToolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
         //Initialize Bottom Navigation View.
-        BottomNavigationView navView = findViewById(R.id.bottom_navigation_view);
+        mBottomNavigationView = findViewById(R.id.bottom_navigation_view);
 
         //Pass the ID's of Different destinations
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_map_view, R.id.navigation_list_view, R.id.navigation_workmates)
                 .build();
 
         //Initialize NavController.
-        NavController navController = Navigation.findNavController(this, R.id.navHostFragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        mNavController = Navigation.findNavController(this, R.id.navHostFragment);
+        NavigationUI.setupActionBarWithNavController(this, mNavController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(mBottomNavigationView, mNavController);
 
     }
 }
