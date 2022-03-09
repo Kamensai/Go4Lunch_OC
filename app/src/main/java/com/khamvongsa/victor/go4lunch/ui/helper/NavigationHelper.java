734c823963e72c649e2c647c20d637c04eee310f
Victor <khamvongsa.v@hotmail.fr>
2022-03-09 12:13:55 +0100

    création du navigationHelper

--- app/src/main/java/com/khamvongsa/victor/go4lunch/ui/helper/NavigationHelper.java
+++ app/src/main/java/com/khamvongsa/victor/go4lunch/ui/helper/NavigationHelper.java
@@ -1,7 +1,9 @@
 package com.khamvongsa.victor.go4lunch.ui.helper;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 /**
  * Created by <Victor Khamvongsa> on <09/03/2022>
  */
-public class NavigationHelper {
+public class NavigationHelper extends AppCompatActivity {
 }
