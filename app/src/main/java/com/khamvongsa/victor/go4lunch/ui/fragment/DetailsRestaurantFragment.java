f29dc2d6ef02bf14057a2ea3d6ca26580f718e23
Victor <khamvongsa.v@hotmail.fr>
2022-06-24 15:36:33 +0200

    fragments, adapters and viewholder created but not finished

---
+++ app/src/main/java/com/khamvongsa/victor/go4lunch/ui/fragment/DetailsRestaurantFragment.java
@@ -0,0 +1,7 @@
+package com.khamvongsa.victor.go4lunch.ui.fragment;
+
+/**
+ * Created by <Victor Khamvongsa> on <24/06/2022>
+ */
+public class DetailsRestaurantFragment {
+}
---
+++ app/src/main/java/com/khamvongsa/victor/go4lunch/ui/views/RestaurantViewHolder.java
@@ -0,0 +1,7 @@
+package com.khamvongsa.victor.go4lunch.ui.views;
+
+/**
+ * Created by <Victor Khamvongsa> on <24/06/2022>
+ */
+public class RestaurantViewHolder {
+}
